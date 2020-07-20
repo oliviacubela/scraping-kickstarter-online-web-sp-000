2f4e0a8a9bd26c98b5c02adc42951870615c56f1
Olivia Cubela <oliviacubela@gmail.com>
2020-07-20 01:58:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
-require 'nokogiri'
-
-html = File.read('fixtures/kickstarter.html')
-
-kickstarter = Nokogiri::HTML(html)
+require "nokogiri"
+require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
