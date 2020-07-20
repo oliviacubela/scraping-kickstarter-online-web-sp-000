724a5278a34a1c5c7a746a7fda80f968096f5c23
Olivia Cubela <oliviacubela@gmail.com>
2020-07-20 02:57:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
